550271a2c7ba67118f98cfb7e2c7f41848207f80
Roy K. Maestre <roy.maestre@udea.edu.co>
2022-02-17 22:30:00 -0500

    Ready to Upload
    As ready as it can be.

--- Lab1Try5-web/src/main/java/com/udea/controller/ClientMBean.java
+++ Lab1Try5-web/src/main/java/com/udea/controller/ClientMBean.java
@@ -13,6 +13,13 @@ import com.udea.entity.Paymentinfo;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
 import javax.ejb.EJB;
 
 /**
@@ -41,6 +48,8 @@ public class ClientMBean implements Serializable {
     private Client client;
     private Paymentinfo paymentinfo;
     private BuyOrder buyOrder;
+    private String cvv;
+    private List<Client> clients;
 
     
     public Client getClientInfo(){
@@ -69,6 +78,134 @@ public class ClientMBean implements Serializable {
     }
     
     public String pay(){
+        System.out.println("PAY THIS SHET");
         return "PAY";
+    }
+    
+    public String cancel(){
+        return "CANCEL";
+    }
+    
+    public String payAgain(){
+        return "PAY_AGAIN";
+    }
+    
+    public String getCvv(){
+        return cvv;
+    }
+    
+    public void setCvv(String cvv){
+        int num;
+        try{
+            num = Integer.parseInt(cvv);
+        }catch(NumberFormatException nfe){
+            return;
+        }
+        if(num >= 100 && num <= 999){
+            this.cvv = cvv;
+        }
+    }
+    
+        public String pay(String orderCost, String email, String username, String phoneNumber,
+            String cardNumber, String expDate, String cvv, String cardOwner){
+        
+        Client c = new Client(username, email);
+        c.setPhoneNumber(Long.parseLong(phoneNumber)); 
+        client=c;
+        
+        Paymentinfo pi = null;
+        if(checkCreditCard(cardNumber)){
+            pi = new Paymentinfo(Long.parseLong(cardNumber),
+                    formatExpDate(expDate), cardOwner);
+            paymentinfo=pi;
+        }
+        
+        BuyOrder bo = new BuyOrder(0000, BigDecimal.valueOf(Double.parseDouble(orderCost)));
+        bo.setTimestamp(new Date());
+        buyOrder = bo;
+        if(checkCVV(cvv) && checkCost(orderCost)){
+            completePay(c, pi, bo);
+            System.out.print("YOOO");
+            return "PAY";
+        }
+        return "PAY";
+    }
+    
+    private void completePay(Client c, Paymentinfo pi, BuyOrder bo){
+        if(!clients.contains(c)){
+            clientManager.update(c);
+        }
+        if(!paymentinfo.equals(pi)){
+            paymentinfoManager.savePayment(paymentinfo);
+        }
+        buyOrderManager.saveBuyOrder(bo);
+    }
+    
+        public List<Client> getClients(){
+        if((clients==null) || clients.isEmpty()){
+            refresh();
+        }
+        return clients;
+    }
+        
+    private void refresh(){
+        clients = clientManager.getAllClients();
+    }
+    
+    private boolean checkCreditCard(String cardNumber){
+        Long number;
+        try{
+            number = Long.parseLong(cardNumber);
+        }catch(NumberFormatException nfe){
+            return false;
+        }
+        return cardNumber.length() >= 12;
+    }
+    
+  
+    private Date formatExpDate(String expDate){        
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("MM/uuuu");
+        LocalDate ldate = YearMonth.parse(expDate, f)
+                .atDay(1);
+        Date date = Date.from(ldate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        return date;
+    }
+    
+    private boolean checkCVV(String cvv){
+        int number;
+        try{
+            number = Integer.parseInt(cvv);
+        }catch(NumberFormatException nfe){
+            return false;
+        }
+        return number >= 100 && number <=999;
+    }
+    
+    private boolean checkCost(String cost){
+        BigDecimal number;
+        try{
+            number = BigDecimal.valueOf(Double.parseDouble(cost));
+        }catch(NumberFormatException nfe){
+            return false;
+        }
+        BigDecimal low = new BigDecimal("500.00");
+        BigDecimal high = new BigDecimal("10000.00");
+        return (number.compareTo(low) + high.compareTo(number)) == 2;
+    }
+    
+    public String checkCardNumber(String cardNumber){
+        int starts = Integer.parseInt(cardNumber.substring(0, 5)); 
+        String brand = "";
+        if(starts >= 11111 && starts <= 22222) brand = "American Express";
+        else if(starts >= 33334 && starts <= 44444) brand = "Diners";
+        else if(starts >= 55555 && starts <= 66666) brand = "Visa";
+        else if(starts >= 77777 && starts <= 88888) brand = "Mastercard";
+        return brand;
+    }
+    
+    public String getCardAlias(){
+        String s = paymentinfo.getCardNumber()
+                .toString();
+        return "Tarjeta terminada en " + s.substring(s.length()-4, s.length());
     }
 }
